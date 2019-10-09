ea14c88ea41ea8fe08f3363b62b600a5e4a9bab0
Ромащенко Н.А <kolya.romashenko@yandex.ru>
2019-10-09 16:57:23 +0300

    Initial commit

---
+++ Forms/FormMain.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace MS_Lab1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelAboutTime = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.textBoxAboutTask = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxAvgSpeed = new System.Windows.Forms.TextBox();
+            this.textBoxConeyorIdle = new System.Windows.Forms.TextBox();
+            this.textBoxAvgSpeedQueue1 = new System.Windows.Forms.TextBox();
+            this.textBoxMaxTimeIdleQueue1 = new System.Windows.Forms.TextBox();
+            this.textBoxMinTimeIdleQueue1 = new System.Windows.Forms.TextBox();
+            this.textBoxAvgSpeedQueue2 = new System.Windows.Forms.TextBox();
+            this.textBoxMaxTimeIdleQueue2 = new System.Windows.Forms.TextBox();
+            this.textBoxMinTimeIdleQueue2 = new System.Windows.Forms.TextBox();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.queue1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.queue2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countSection1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countSection2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isEquipmentFree = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.equipment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            resources.ApplyResources(this.buttonStart, "buttonStart");
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // labelAboutTime
+            // 
+            resources.ApplyResources(this.labelAboutTime, "labelAboutTime");
+            this.labelAboutTime.Name = "labelAboutTime";
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.time,
+            this.queue1,
+            this.queue2,
+            this.countSection1,
+            this.countSection2,
+            this.isEquipmentFree,
+            this.equipment});
+            this.dgv.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            resources.ApplyResources(this.dgv, "dgv");
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            // 
+            // textBoxTime
+            // 
+            resources.ApplyResources(this.textBoxTime, "textBoxTime");
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.ReadOnly = true;
+            // 
+            // buttonHelp
+            // 
+            resources.ApplyResources(this.buttonHelp, "buttonHelp");
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.ButtonHelp_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // buttonExit
+            // 
+            resources.ApplyResources(this.buttonExit, "buttonExit");
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.ButtonExit_Click);
+            // 
+            // textBoxAboutTask
+            // 
+            resources.ApplyResources(this.textBoxAboutTask, "textBoxAboutTask");
+            this.textBoxAboutTask.Name = "textBoxAboutTask";
+            this.textBoxAboutTask.ReadOnly = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // textBoxAvgSpeed
+            // 
+            resources.ApplyResources(this.textBoxAvgSpeed, "textBoxAvgSpeed");
+            this.textBoxAvgSpeed.Name = "textBoxAvgSpeed";
+            this.textBoxAvgSpeed.ReadOnly = true;
+            // 
+            // textBoxConeyorIdle
+            // 
+            resources.ApplyResources(this.textBoxConeyorIdle, "textBoxConeyorIdle");
+            this.textBoxConeyorIdle.Name = "textBoxConeyorIdle";
+            this.textBoxConeyorIdle.ReadOnly = true;
+            // 
+            // textBoxAvgSpeedQueue1
+            // 
+            resources.ApplyResources(this.textBoxAvgSpeedQueue1, "textBoxAvgSpeedQueue1");
+            this.textBoxAvgSpeedQueue1.Name = "textBoxAvgSpeedQueue1";
+            this.textBoxAvgSpeedQueue1.ReadOnly = true;
+            // 
+            // textBoxMaxTimeIdleQueue1
+            // 
+            resources.ApplyResources(this.textBoxMaxTimeIdleQueue1, "textBoxMaxTimeIdleQueue1");
+            this.textBoxMaxTimeIdleQueue1.Name = "textBoxMaxTimeIdleQueue1";
+            this.textBoxMaxTimeIdleQueue1.ReadOnly = true;
+            // 
+            // textBoxMinTimeIdleQueue1
+            // 
+            resources.ApplyResources(this.textBoxMinTimeIdleQueue1, "textBoxMinTimeIdleQueue1");
+            this.textBoxMinTimeIdleQueue1.Name = "textBoxMinTimeIdleQueue1";
+            this.textBoxMinTimeIdleQueue1.ReadOnly = true;
+            // 
+            // textBoxAvgSpeedQueue2
+            // 
+            resources.ApplyResources(this.textBoxAvgSpeedQueue2, "textBoxAvgSpeedQueue2");
+            this.textBoxAvgSpeedQueue2.Name = "textBoxAvgSpeedQueue2";
+            this.textBoxAvgSpeedQueue2.ReadOnly = true;
+            // 
+            // textBoxMaxTimeIdleQueue2
+            // 
+            resources.ApplyResources(this.textBoxMaxTimeIdleQueue2, "textBoxMaxTimeIdleQueue2");
+            this.textBoxMaxTimeIdleQueue2.Name = "textBoxMaxTimeIdleQueue2";
+            this.textBoxMaxTimeIdleQueue2.ReadOnly = true;
+            // 
+            // textBoxMinTimeIdleQueue2
+            // 
+            resources.ApplyResources(this.textBoxMinTimeIdleQueue2, "textBoxMinTimeIdleQueue2");
+            this.textBoxMinTimeIdleQueue2.Name = "textBoxMinTimeIdleQueue2";
+            this.textBoxMinTimeIdleQueue2.ReadOnly = true;
+            // 
+            // time
+            // 
+            this.time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            resources.ApplyResources(this.time, "time");
+            this.time.Name = "time";
+            this.time.ReadOnly = true;
+            // 
+            // queue1
+            // 
+            this.queue1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            resources.ApplyResources(this.queue1, "queue1");
+            this.queue1.Name = "queue1";
+            this.queue1.ReadOnly = true;
+            // 
+            // queue2
+            // 
+            this.queue2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            resources.ApplyResources(this.queue2, "queue2");
+            this.queue2.Name = "queue2";
+            this.queue2.ReadOnly = true;
+            // 
+            // countSection1
+            // 
+            this.countSection1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            resources.ApplyResources(this.countSection1, "countSection1");
+            this.countSection1.Name = "countSection1";
+            this.countSection1.ReadOnly = true;
+            // 
+            // countSection2
+            // 
+            this.countSection2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            resources.ApplyResources(this.countSection2, "countSection2");
+            this.countSection2.Name = "countSection2";
+            this.countSection2.ReadOnly = true;
+            // 
+            // isEquipmentFree
+            // 
+            this.isEquipmentFree.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            resources.ApplyResources(this.isEquipmentFree, "isEquipmentFree");
+            this.isEquipmentFree.Name = "isEquipmentFree";
+            this.isEquipmentFree.ReadOnly = true;
+            // 
+            // equipment
+            // 
+            this.equipment.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            resources.ApplyResources(this.equipment, "equipment");
+            this.equipment.Name = "equipment";
+            this.equipment.ReadOnly = true;
+            // 
+            // FormMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBoxMinTimeIdleQueue2);
+            this.Controls.Add(this.textBoxMaxTimeIdleQueue2);
+            this.Controls.Add(this.textBoxAvgSpeedQueue2);
+            this.Controls.Add(this.textBoxMinTimeIdleQueue1);
+            this.Controls.Add(this.textBoxMaxTimeIdleQueue1);
+            this.Controls.Add(this.textBoxAvgSpeedQueue1);
+            this.Controls.Add(this.textBoxConeyorIdle);
+            this.Controls.Add(this.textBoxAvgSpeed);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.textBoxTime);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.labelAboutTime);
+            this.Controls.Add(this.textBoxAboutTask);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelAboutTime;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox textBoxAboutTask;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxAvgSpeed;
+        private System.Windows.Forms.TextBox textBoxConeyorIdle;
+        private System.Windows.Forms.TextBox textBoxAvgSpeedQueue1;
+        private System.Windows.Forms.TextBox textBoxMaxTimeIdleQueue1;
+        private System.Windows.Forms.TextBox textBoxMinTimeIdleQueue1;
+        private System.Windows.Forms.TextBox textBoxAvgSpeedQueue2;
+        private System.Windows.Forms.TextBox textBoxMaxTimeIdleQueue2;
+        private System.Windows.Forms.TextBox textBoxMinTimeIdleQueue2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn queue1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn queue2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countSection1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countSection2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn isEquipmentFree;
+        private System.Windows.Forms.DataGridViewTextBoxColumn equipment;
+    }
+}
+
---
+++ Forms/FormMain.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MS_Lab1
+{
+    public partial class FormMain : Form
+    {
+        float t0 = 0; // стартовое время системы (мин)
+        float dt = 0; // приращение времени  (мин)
+
+        const float t1 = 5; // время для 1 детали (мин)
+        const float t2 = 20; // время для 2 детали (мин)
+        const float t3 = 10; // время для сборки (мин)
+
+        const int N1 = 5; // шт. деталей выплевывается из 1 блока
+        const int N2 = 20; // шт. деталей выплевывается из 2 блока
+        const int N3 = 10;  // шт. деталей надо для конвейера
+
+        // Инициализация главной формы
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // Сортируем времена по возрастанию
+            List<float> list = new List<float>();
+            list.Add(t1);
+            list.Add(t2);
+            list.Add(t3);
+            list.Sort();
+
+            dt = list[0] / 10; // делим наименьшее время на 10 (так надо)
+        }
+        // Обработка кнопки "Выход" на 1 вкладке
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Конец модуляции наступает, когда пройдут 1 сутки
+        // 24 часа * 60 мин
+        float isTimeOut = 24f*60f;
+
+        float _t1 = t1;
+        float _t2 = t2;
+        float _t3 = t3;
+
+        // Начать моделирование
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            // Обнуление переменных
+            t0 = 0; // стартовое время системы (мин)
+
+            _t1 = 5; // время для 1 детали (мин)
+            _t2 = 20; // время для 2 детали (мин)
+            _t3 = 10; // время для сборки (мин)
+
+            // Очередь 1
+            int queue1 = 0;
+            // Очередь 2
+            int queue2 = 0;
+            // Комплектация 
+            int equipment = 0;
+            // Наличие деталей в 1 секции 
+            int countSection1 = 0;
+            // Наличие деталей во 2 секции 
+            int countSection2 = 0;
+            // Сборка свободна
+            bool isEquipmentFree = true;
+            // Время завершения сборки
+            float tFinish = 0;
+
+            //////////////////
+            ///
+            /// Показатели работы:
+            ///
+            //////////////////
+
+            // Таймер
+            var timer = Stopwatch.StartNew();
+            // Средняя производительность конвейера
+            float avgMakeDetal = 0;
+            // Полное время простоя конвейера из-за незаполненности секций
+            float fullIdleTimeConveyor = 0;
+            // Средние и максимальное очереди по каждому типу изделия
+            float avgQueue1 = 0;
+            float maxQueue1 = 0;
+            float minQueue1 = 0;
+            float avgQueue2 = 0;
+            float maxQueue2 = 0;
+            float minQueue2 = 0;
+            // Для среднего значения:
+            List<int> queue1Elems = new List<int>();
+            List<int> queue2Elems = new List<int>();
+
+            // Начало модуляции
+            while (t0 <= isTimeOut)
+            {
+                // УЗ 1
+                if (t0 >= _t1)
+                {
+                    // Суем в очередь изготовленные детали 1 типа
+                    queue1 += N1;
+                    // Считаем следующее время для изготовление детали 1 типа
+                    _t1 = t0 + t1;
+                }
+                // УЗ 2
+                if (t0 >= _t2)
+                {
+                    // Суем в очередь изготовленные детали 1 типа
+                    queue2 += N2;
+                    // Считаем следующее время для изготовление детали 1 типа
+                    _t2 = t0 + t2;
+                }
+                // УЗ 3
+                if ((queue1 > 0 && countSection1 < N3) || (queue2 > 0 &&  countSection2 < N3))
+                {
+                    // Если в очереди 1 есть что-то и в конвейере меньше N3 деталей, то кидаем детали в конвейер
+                    if (queue1 > 0 && countSection1 < N3)
+                    {
+                        do
+                        {
+                            ++countSection1;
+                            --queue1;
+                        } while (queue1 > 0 && countSection1 != N3);
+                    }
+                    // Если в очереди 2 есть что-то и в конвейере меньше N3 деталей, то кидаем детали в конвейер
+                    if (queue2 > 0 && countSection2 < N3)
+                    {
+                        do
+                        {
+                            ++countSection2;
+                            --queue2;
+                        } while (queue2 > 0 && countSection2 != N3);
+                    }
+                }
+                // УЗ 4
+                if (countSection1 == N3 && countSection2 == N3 && isEquipmentFree)
+                {
+                    // Сборка занята
+                    isEquipmentFree = false;
+                    // Рассчет времени завершения
+                    tFinish = t0 + t3;
+                    // Забираем все детали из конвейера
+                    countSection1 = 0;
+                    countSection2 = 0;
+                }
+                // УЗ 5
+                if (tFinish >= t0 && tFinish < (t0 + dt) && t0 != 0f)
+                {
+                    // Создаем новую вещь
+                    ++equipment;
+                    // Сборка свободна
+                    isEquipmentFree = true;
+                }
+                // Вычисление простоя конвейера
+                if (countSection1 == 0 || countSection2 == 0)
+                {
+                    fullIdleTimeConveyor += dt;
+                }
+                // Вычисление max/min/avg очередей
+                if (countSection1 == N3)
+                {
+                    maxQueue1 = Math.Max(maxQueue1, queue1);
+                    minQueue1 = Math.Min(minQueue1, queue1);
+                    queue1Elems.Add(queue1);
+                }
+                if (countSection2 == N3)
+                {
+                    maxQueue2 = Math.Max(maxQueue2, queue2);
+                    minQueue2 = Math.Min(minQueue2, queue2);
+                    queue2Elems.Add(queue2);
+                }
+
+                // добавление в dgv инфы
+                int rowNumber = dgv.Rows.Add();
+                dgv.Rows[rowNumber].Cells["time"].Value = t0;
+                dgv.Rows[rowNumber].Cells["queue1"].Value = queue1;
+                dgv.Rows[rowNumber].Cells["queue2"].Value = queue2;
+                dgv.Rows[rowNumber].Cells["countSection1"].Value = countSection1;
+                dgv.Rows[rowNumber].Cells["countSection2"].Value = countSection2;
+                dgv.Rows[rowNumber].Cells["isEquipmentFree"].Value = !isEquipmentFree;
+                dgv.Rows[rowNumber].Cells["equipment"].Value = equipment;
+
+                // Прибавление времени
+                t0 += dt;
+            }
+            // Остановка таймера
+            timer.Stop();
+            // Показ на форме
+            textBoxTime.Text = String.Format("{0:0.00}", ((float)timer.ElapsedMilliseconds) / 1000 / 60); // в минутах
+
+            // Средняя производительность конвейера
+            avgMakeDetal = (float)equipment / (t0 / 60f / 24f); // деталей в сутки
+            // Запись на форму
+            textBoxAvgSpeed.Text = avgMakeDetal.ToString();
+
+            // Полное время простоя конвейера
+            textBoxConeyorIdle.Text = fullIdleTimeConveyor.ToString();
+
+            // Средние и максимальное очереди по каждому типу изделия
+            textBoxMaxTimeIdleQueue1.Text = maxQueue1.ToString();
+            textBoxMaxTimeIdleQueue2.Text = maxQueue2.ToString();
+            textBoxMinTimeIdleQueue1.Text = minQueue1.ToString();
+            textBoxMinTimeIdleQueue2.Text = minQueue2.ToString();
+            // Среднее значение:
+            foreach (int q in queue1Elems)
+            {
+                avgQueue1 += q;
+            }
+            foreach (int q in queue2Elems)
+            {
+                avgQueue2 += q;
+            }
+            avgQueue1 /= (float)queue1Elems.Count;
+            avgQueue2 /= (float)queue1Elems.Count;
+            // Вывод в форму
+            textBoxAvgSpeedQueue1.Text = avgQueue1.ToString();
+            textBoxAvgSpeedQueue2.Text = avgQueue2.ToString();
+        }
+
+        private void ButtonHelp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
